336c1c1d38952abc6c6558b00764708e4bfb3b65
Murilo Coelho <m.coelhos1212@gmail.com>
2023-09-17 18:48:58 -0300

    backend application

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { fastify } from "fastify"
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/getAllPrompts"
+import { uploadVideoRoute } from "./routes/uploadVideo"
+import { createTranscriptionRoute } from "./routes/createTranscription"
+import { generateAICompletionRoute } from "./routes/generation-ai-completion"
+
+const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log("Server online")
+})
